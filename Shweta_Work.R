cfe032da1dbbf1a812e515d5c6b5206f4abcbb9a
desaism <shweta.desai@adp.com>
2018-04-25 21:53:00 -0700

    Adding inital reserch for dot plot combining two graphs

---
+++ Shweta_Work.R
@@ -0,0 +1,12 @@
+
+### Take the happiness dataset and draw the happniess dot plot graph
+
+##  Draw the CPI graph
+
+## Combined to graph
+
+## samples : https://www.r-bloggers.com/r-single-plot-with-two-different-y-axes/
+## samples : https://stackoverflow.com/questions/3099219/plot-with-2-y-axes-one-y-axis-on-the-left-and-another-y-axis-on-the-right?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+## samples : Good links: https://stackoverflow.com/questions/21192002/how-to-combine-2-plots-ggplot-into-one-plot?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+
+
